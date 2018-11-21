d6da616f3daf0828e18a119c3cd98c7be117800f
ddSoul <972424506@qq.com>
2018-11-21 16:42:10 +0800

    add NSDictionary

---
+++ XLCrashDemo/XLCrashDemo/XLCrash/NSDictionary+NilSafe.h
@@ -0,0 +1,21 @@
+//
+//  NSDictionary+NilSafe.h
+//  XLListViewDemo
+//
+//  Created by ddSoul on 2018/11/21.
+//  Copyright © 2018 dxl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSDictionary (NilSafe)
+
+@end
+
+@interface NSMutableDictionary (NilSafe)
+
+@end
+
+NS_ASSUME_NONNULL_END
